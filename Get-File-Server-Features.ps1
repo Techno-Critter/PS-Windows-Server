b4be858c393e9ffca887a071d915586489a91af5
TechnoCritter <34747092+Techno-Critter@users.noreply.github.com>
2020-04-28 07:43:59 -0600

    Update Get-File-Server-Features.ps1
    Added shares worksheet, added DHCP property, added center header row function

--- Get-File-Server-Features.ps1
+++ Get-File-Server-Features.ps1
@@ -6,7 +6,13 @@ Crap: gets servers from specified AD Group and retreives file server roles and f
 ### https://github.com/dfinke/ImportExcel
 #>
 
-# Function: Convert drive sizes to legible strings
+# Configure variables
+$Date = Get-Date -Format yyyyMMdd
+$LogFile = "C:\Reports\File Servers\File_Servers_$Date.xlsx"
+$ADGroup = "CN=ServerGroup,DC=ACME,DC=COM"
+
+## Functions
+#Convert drive sizes to legible strings
 Function Get-Size([double]$DataSize) {
     Switch ($DataSize) {
         { $_ -lt 1KB } {
@@ -31,19 +37,74 @@ Function Get-Size([double]$DataSize) {
     $DataValue
 }
 
-# Configure variables
-$Date = Get-Date -Format yyyyMMdd
-$LogFile = "C:\Reports\File Servers\File_Servers_$Date.xlsx"
+# Convert number of object items into Excel column headers
+Function Get-ColumnName ([int]$ColumnCount){
+    If(($ColumnCount -le 702) -and ($ColumnCount -ge 1)){
+        $ColumnCount = [Math]::Floor($ColumnCount)
+        $CharStart = 64
+        $FirstCharacter = $null
 
+        # Convert number into double letter column name (AA-ZZ)
+        If($ColumnCount -gt 26){
+            $FirstNumber = [Math]::Floor(($ColumnCount)/26)
+            $SecondNumber = ($ColumnCount) % 26
+
+            # Reset increment for base-26
+            If($SecondNumber -eq 0){
+                $FirstNumber--
+                $SecondNumber = 26
+            }
+
+            # Left-side column letter (first character from left to right)
+            $FirstLetter = [int]($FirstNumber + $CharStart)
+            $FirstCharacter = [char]$FirstLetter
+
+            # Right-side column letter (second character from left to right)
+            $SecondLetter = $SecondNumber + $CharStart
+            $SecondCharacter = [char]$SecondLetter
+
+            # Combine both letters into column name
+            $CharacterOutput = $FirstCharacter + $SecondCharacter
+        }
+
+        # Convert number into single letter column name (A-Z)
+        Else{
+            $CharacterOutput = [char]($ColumnCount + $CharStart)
+        }
+    }
+    Else{
+        $CharacterOutput = "ZZ"
+    }
+
+    # Output column name
+    $CharacterOutput
+}
+
+## Script below
 If (Test-Path $LogFile) {
     Write-Warning "The file $LogFile already exists. Script terminated."
 }
 Else {
     # Configure parameters and arrays
-    $ServerList = Get-ADGroupMember -Identity "CN=File Servers,DC=acme,DC=com" | Select-Object * | Sort-Object Name
+    $ServerList = Get-ADGroupMember -Identity $ADGroup | Select-Object * | Sort-Object Name
     $ServerFeatureArray = @()
     $ServerDriveArray = @()
+    $ServerShareArray = @()
     $ErrorArray = @()
+
+    # Exclude default shares based on names and descriptions
+    $ShareDescriptionExclusions = "Default share",
+        "Remote IPC",
+        "Remote Admin",
+        "RemoteInstallation"
+    $ShareNameExclusions = "SMS_DP$",
+        "SMSPKGD$",
+        "SMSPKGE$",
+        "SCCMContentLib$",
+        "SMSSIG$",
+        "MTATempStore$",
+        "print$",
+        "prnproc$"
 
     # Search through list
     ForEach ($Member in $ServerList) {
@@ -55,6 +116,8 @@ Else {
         $Dedup = $null
         $ScheduledTasks = $null
         $ShadowCopyVolumes = $null
+        $Shares = $null
+        $SMBAccess = $null
         Write-Output ("Processing $($Member.Name)...")
 
         $Server = Get-ADComputer -Identity $Member.DistinguishedName -Properties *
@@ -74,6 +137,7 @@ Else {
                         "DFS Namespace"  = ($Features | Where-Object { $_.Name -eq "FS-DFS-Namespace" }).InstallState
                         "FSRM"           = ($Features | Where-Object { $_.Name -eq "FS-Resource-Manager" }).InstallState
                         "Search Service" = ($Features | Where-Object { $_.Name -eq "Search-Service" }).InstallState
+                        "DHCP"           = ($Features | Where-Object {$_.Name -eq "DHCP"}).InstallState
                     }
                 }
             }
@@ -84,20 +148,24 @@ Else {
                     "Error"  = $_.Exception.Message
                 }
             }
+            
             Try {
+                # Call server properties
                 $ServerSession = New-CimSession -ComputerName $Server.Name -ErrorAction Stop
                 $OpSys = Get-CimInstance Win32_OperatingSystem -CimSession $ServerSession -ErrorAction SilentlyContinue
                 $Drives = Get-CimInstance Win32_LogicalDisk -CimSession $ServerSession -Filter "DriveType='3'" -ErrorAction SilentlyContinue
                 $Volumes = Get-CimInstance Win32_Volume -CimSession $ServerSession -ErrorAction SilentlyContinue
                 $Dedup = Invoke-Command -ComputerName $Server.Name -ScriptBlock{Get-DeDupStatus} -ErrorAction SilentlyContinue
                 $ScheduledTasks = Get-ScheduledTask -CimSession $ServerSession -TaskName ShadowCopyVolume* -ErrorAction SilentlyContinue
-                Remove-CimSession -CimSession $ServerSession
+                $Shares = Get-CimInstance Win32_Share -CimSession $ServerSession | Where-Object{($_.Description -notin $ShareDescriptionExclusions) -and ($_.Name -notin $ShareNameExclusions)}
 
+                # Server Volume Shadow Copies
                 $ShadowCopyVolumes = $null
                 If ($ScheduledTasks) {
                     $ShadowCopyVolumes = $ScheduledTasks.Actions.Arguments | ForEach-Object { $_.Split()[3].Split("=")[1] }
                 }
 
+                # Server Drives
                 ForEach ($Drive in $Drives) {
                     $DedupStatus = "Disabled"
                     $VolumeID = ($Volumes | Where-Object { $_.DriveLetter -eq $Drive.DeviceID }).DeviceID
@@ -138,12 +206,31 @@ Else {
                         "Volume ID"         = $VolumeID
                     }
                 }
+                
+                # Server Shares
+                ForEach($Share in $Shares){
+                    $SMBAccess = Get-SmbShareAccess -Name $Share.Name -CimSession $ServerSession
+                    ForEach($SMBObject in $SMBAccess){
+                        $ServerShareArray += [PSCustomObject]@{
+                            "Server"       = $Server.Name
+                            "Share Name"   = $Share.Name
+                            "Share Path"   = $Share.Path
+                            "Description"  = $Share.Description
+                            "Account Name" = $SMBObject.AccountName
+                            "Access Right" = $SMBObject.AccessRight
+                            "Control Type" = $SMBObject.AccessControlType
+                        }
+                    }
+                }
+                
+                # Close server connection
+                Remove-CimSession -CimSession $ServerSession
             }
             Catch {
                 $ErrorArray += [PSCustomObject]@{
-                    "Server" = $Server.Name
+                    "Server"  = $Server.Name
                     "Command" = $_.Exception.Command
-                    "Error"  = $_.Exception.Message
+                    "Error"   = $_.Exception.Message
                 }
             }
         }
@@ -162,14 +249,15 @@ Else {
                 "DFS Namespace"  = "N/A"
                 "FSRM"           = "N/A"
                 "Search Service" = "N/A"
+                "DHCP"           = "N/A"
             }
         }
     }
 
-    # Export to Excel
-    $HeaderRow = ("!`$A`$1:`$Z`$1")
-
+    ## Export to Excel
     # File Servers worksheet
+    $ServerSheetHeaderCount = Get-ColumnName ($ServerFeatureArray | Get-Member | Where-Object{$_.MemberType -match "NoteProperty"} | Measure-Object).Count
+    $ServerSheetHeaderRow = "`$A`$1:`$$ServerSheetHeaderCount`$1"
     $ServerSheetLastRow = ($ServerFeatureArray | Measure-Object).Count + 1
     If($ServerSheetLastRow -gt 1){
         $FileServerColumn = "`$F`$2:`$F`$$ServerSheetLastRow"
@@ -180,7 +268,7 @@ Else {
         $SearchServiceColumn = "`$K`$2:`$K`$$ServerSheetLastRow"
 
         $ServerFeatureStyle = @()
-        $ServerFeatureStyle += New-ExcelStyle -Range "'File Servers'$HeaderRow" -HorizontalAlignment Center
+        $ServerFeatureStyle += New-ExcelStyle -Range "'File Servers'$ServerSheetHeaderRow" -HorizontalAlignment Center
 
         $ServerFeatureCondText = @()
         $ServerFeatureCondText += New-ConditionalText -Range $FileServerColumn -ConditionalType ContainsText "Installed" -ConditionalTextColor Green -BackgroundColor LightGreen
@@ -192,22 +280,26 @@ Else {
 
         $ServerFeatureArray | Sort-Object "Server" | Export-Excel -Path $LogFile -AutoSize -FreezeTopRow -BoldTopRow -WorkSheetname "File Servers" -ConditionalText $ServerFeatureCondText -Style $ServerFeatureStyle
     }
+
     # Server Drives worksheet
+    $DriveSheetHeaderCount = Get-ColumnName ($ServerDriveArray | Get-Member | Where-Object{$_.MemberType -match "NoteProperty"} | Measure-Object).Count
+    $DriveSheetHeaderRow = "`$A`$1:`$$DriveSheetHeaderCount`$1"
     $DriveSheetLastRow = ($ServerDriveArray | Measure-Object).Count + 1
-    If ($DriveSheetLastRow -gt 1) {
+    If($DriveSheetLastRow -gt 1){
         $DriveRawSizeColumn = "'Drive!'`$H`$2:`$H`$$DriveSheetLastRow"
         $DriveRawFreeColumn = "'Drive!'`$I`$2:`$I`$$DriveSheetLastRow"
         $DriveRawSavingsColumn = "'Drive!'`$J`$2:`$J`$$DriveSheetLastRow"
-        #$DriveSystemDrColumn = "`$D`$2:`$D`$$DriveSheetLastRow"
+        $DriveSystemDrColumn = "`$D`$2:`$D`$$DriveSheetLastRow"
         $DriveDedupColumn = "`$K`$2:`$K`$$DriveSheetLastRow"
         $DriveShadowColumn = "`$L`$2:`$L`$$DriveSheetLastRow"
 
         $DriveCondText = @()
+        $DriveCondText += New-ConditionalText -Range $DriveSystemDrColumn -ConditionalType ContainsText "TRUE" -ConditionalTextColor Navy -BackgroundColor Cyan
         $DriveCondText += New-ConditionalText -Range $DriveDedupColumn -ConditionalType Expression "=AND(`$D2=TRUE,`$L2=TRUE)" -ConditionalTextColor Maroon -BackgroundColor Pink
         $DriveCondText += New-ConditionalText -Range $DriveShadowColumn -ConditionalType Expression "=AND(`$D2=FALSE,`$L2=FALSE)" -ConditionalTextColor Maroon -BackgroundColor Pink
 
         $DriveStyle = @()
-        $DriveStyle += New-ExcelStyle -Range "'Server Drives'$HeaderRow" -HorizontalAlignment Center
+        $DriveStyle += New-ExcelStyle -Range "'Server Drives'$DriveSheetHeaderRow" -HorizontalAlignment Center
         $DriveStyle += New-ExcelStyle -Range $DriveRawSizeColumn -NumberFormat 0
         $DriveStyle += New-ExcelStyle -Range $DriveRawFreeColumn -NumberFormat 0
         $DriveStyle += New-ExcelStyle -Range $DriveRawSavingsColumn -NumberFormat 0
@@ -215,9 +307,22 @@ Else {
         $ServerDriveArray | Sort-Object "Server", "Drive" | Export-Excel -Path $LogFile -AutoSize -FreezeTopRow -BoldTopRow -WorkSheetname "Server Drives" -ConditionalText $DriveCondText -Style $DriveStyle
     }
 
+    # Server Share worksheet
+    $ServerSharesHeaderCount = Get-ColumnName ($ServerShareArray | Get-Member | Where-Object{$_.MemberType -match "NoteProperty"} | Measure-Object).Count
+    $ServerSharesHeaderRow = "`$A`$1:`$$ServerSharesHeaderCount`$1"
+    $ServerSharesLastRow = ($ServerShareArray | Measure-Object).Count + 1
+    $SharesControlColumn = "`$G`$2:`$G`$$ServerSharesLastRow"
+    $ServerSharesStyle = @()
+    $ServerSharesStyle += New-ExcelStyle -Range "'Server Shares'$ServerSharesHeaderRow" -HorizontalAlignment Center
+    $ServerSharesConditionalText = @()
+    $ServerSharesConditionalText += New-ConditionalText -Range $SharesControlColumn -ConditionalType ContainsText "Deny" -ConditionalTextColor Maroon -BackgroundColor Pink
+    $ServerShareArray | Sort-Object "Server","Share Path","Account Name" | Export-Excel -Path $LogFile -AutoSize -FreezeTopRow -BoldTopRow -WorkSheetname "Server Shares" -ConditionalText $ServerSharesConditionalText -Style $ServerSharesStyle
+
     # Errors worksheet
-    If ($ErrorArray -ne "") {
-        $ErrorStyle = New-ExcelStyle -Range "'Errors'$HeaderRow" -HorizontalAlignment Center
+    If($ErrorArray -ne ""){
+        $ErrorSheetHeaderCount = Get-ColumnName ($ErrorArray | Get-Member | Where-Object{$_.MemberType -match "NoteProperty"} | Measure-Object).Count
+        $ErrorSheetHeaderRow = "`$A`$1:`$$ErrorSheetHeaderCount`$1"
+        $ErrorStyle = New-ExcelStyle -Range "'Errors'$ErrorSheetHeaderRow" -HorizontalAlignment Center
         $ErrorArray | Export-Excel -Path $LogFile -AutoSize -FreezeTopRow -BoldTopRow -WorkSheetname "Errors" -Style $ErrorStyle
     }
 }
