681f81c2e667a687246597943a12fde2afc17516
TechnoCritter <34747092+Techno-Critter@users.noreply.github.com>
2020-04-28 08:07:11 -0600

    Create Set-ADUser-HomeDrives.ps1
    Creates home directory based on user SAM name and sets permissions to "user/modify" with inheritance and gives ownership to user. If folder already exists, permissions will be changed to "user/modify" with inheritance but will not force propagation. If user already has a home path set that does not meet the script configuration, that path will be changed.

---
+++ Set-ADUser-HomeDrives.ps1
@@ -0,0 +1,42 @@
+<#
+Author: Stan Crider
+Date: 17April2020
+Crap: Assigns and creates home directories for users in specified OU. This will also
+set the permissions on the newly created folder to "modify" for the user account and
+set the ownership to the user account.
+### Must have NTFSSecurity module installed!!!
+### https://github.com/raandree/NTFSSecurity
+#>
+
+$ADLocation = "OU=Users,DC=ACME,DC=COM"
+$HomeDirectory = "\\ACME.COM\"
+$HomeDrive = "H:"
+$Domain = "ACME"
+
+$ADUsers = Get-ADUser -Filter * -SearchBase $ADLocation -Properties HomeDirectory,HomeDrive,SamAccountName,SID
+
+ForEach($User in $ADUsers){
+    If($User.Enabled -eq $true){
+        $NewHomeFolder = ($HomeDirectory + $User.SamAccountName)
+        $FolderUser = "$Domain\$($User.SamAccountName)"
+        $UserPermissions = [System.Security.AccessControl.FileSystemRights]"Modify"
+        $Inheritance = [System.Security.AccessControl.InheritanceFlags]::"ContainerInherit", "ObjectInherit"
+        $Propagation = [System.Security.AccessControl.PropagationFlags]::None
+        $AccessControl =[System.Security.AccessControl.AccessControlType]::Allow
+        $AccessRule = New-Object System.Security.AccessControl.FileSystemAccessRule($FolderUser,$UserPermissions,$Inheritance,$Propagation,$AccessControl)
+
+        If((Test-Path $NewHomeFolder) -eq $false){
+            New-Item -ItemType Directory -Path $NewHomeFolder
+        }
+
+        $FolderOwner = Get-NTFSOwner -Path $NewHomeFolder
+        If($FolderOwner.Account.Sid -ne $User.SID){
+            Set-NTFSOwner -Path $NewHomeFolder -Account $User.SID
+        }
+
+        $FolderACL = Get-Acl -Path $NewHomeFolder
+        $FolderACL.SetAccessRule($AccessRule)
+        Set-Acl -Path $NewHomeFolder -AclObject $FolderACL
+        Set-ADUser -Identity $User -HomeDrive $HomeDrive -HomeDirectory $NewHomeFolder
+    }
+}
